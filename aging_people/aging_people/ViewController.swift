4d3f99f10a958306db3f1aace3b8a1a18805c8e1
jupterorbita <john.williams@outlook.com>
2018-07-09 10:57:56 -0700

    added people rnd age

--- aging_people/aging_people/ViewController.swift
+++ aging_people/aging_people/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var people = ["George", "Betty", "Fran", "John", "Chris", "Jimmy", "Bob"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+
+        cell.textLabel?.text = people[indexPath.row]
+        cell.detailTextLabel?.text = "\(arc4random_uniform(95 - 5))"
+    
+        cell.backgroundColor = UIColor.lightGray
+
+        return cell
+    }
+    
 }
 
